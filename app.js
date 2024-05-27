797bee4ed5aafff00542ac7f0f3c448ff102f04b
Abdulmumin Isah <abdulmuminisah@yahoo.com>
2024-05-27 11:00:27 +0100

    eccomerce app server

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const  express = require("express")
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,()=> console.log("Server is running at port 3000"));
+
+
+
+//js -- language
+// Node.js -- runtime environment
+//Express.js -- framework
